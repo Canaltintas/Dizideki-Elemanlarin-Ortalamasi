732ed481e85b628ccec4536b064779c9f180d482
Can Altıntaş <cans06@icloud.com>
2022-08-17 18:00:28 +0300

    First Commit

---
+++ src/main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class main {
+    static int[] addItem(int[] array){
+
+        for (int i=0;i< array.length;i++){
+
+            array[i]=(int) (Math.random()*100);
+
+
+        }
+        Arrays.sort(array);
+
+        return array;
+    }
+    static double average(int[] array){
+        double sum=0;
+        for (int i:array){
+            sum +=i;
+        }
+        sum /=array.length;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int boundary;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Dizinin kaç elemanlı olacağını giriniz : ");
+        boundary=input.nextInt();
+        int[] array = new int[boundary];
+        addItem(array);
+        System.out.println("Ortalama : "+ average(array));
+        System.out.println(Arrays.toString(array));
+
+
+
+    }
+}
